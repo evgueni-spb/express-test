45638ae7b0ff8152118de614f8fa1b4237b4ecb8
evguenichakine <evgueni.spb@gmail.com>
2020-04-17 17:18:56 +0300

    added update and delete REST methods

--- router/api/members.js
+++ router/api/members.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const members=require("../../Members")
-
+const uuid=require("uuid")
 const router=express.Router()
 
 router.get('/',(req,res) => {
@@ -20,8 +20,58 @@ router.get('/:id',(req,res)=> {
 }
 )
 
+
+//create member
 router.post('/',(req,res)=>{
-    res.send(req.body)
+    const newMember={
+        "id":uuid.v4(),
+        "name":req.body.name,
+        "age":req.body.age
+    }
+
+    if(req.body.name==null || req.body.age==null){
+        res.status(400).json({"msg":"Name and Age are required"})
+        return
+    }
+
+    members.push(newMember)
+    res.json(members)
 })
+
+//update member
+router.put('/:id',(req,res)=> {
+
+    const found=members.some(member => member.id === parseInt(req.params.id))
+    if(found){
+        const updatedMember=req.body
+        members.forEach( function(member) {
+            if(member.id === parseInt(req.params.id)){
+                member.name=member.name === updatedMember.name ? updatedMember.name:member.name
+                member.age=member.age === updatedMember.age ? updatedMember.age:member.age
+
+                res.json({"msg":"Member was updated","member":member})
+                return
+            }
+        });
+    }else{
+        res.status(400).json({message: `No member with ${req.params.id} can be found`})
+    }
+}
+)
+
+//delete member
+router.delete('/:id',(req,res)=> {
+    //res.send(req.params.id)
+
+    const found=members.some(member => member.id === parseInt(req.params.id))
+    if(found){
+        res.json({msg:'Member deleted',mems:members.filter(member => member.id !== parseInt(req.params.id))})
+    }else{
+        res.status(400).json({message: `No member with ${req.params.id} can be found`})
+    }
+}
+)
+
+
 
 module.exports=router
